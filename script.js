6872336a7c754d2ad96d4157d81fa039d04f0817
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-04-08 19:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
 //your JS code here. If required.
+    const btn = document.getElementById("btn");
+    const result = document.getElementById("output");
+
+    function MultipliedByTwo(value) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const multiplied = value * 2;
+          resolve(multiplied);
+        }, 1000);
+      });
+    }
+
+    function SubtractThree(value) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const subtracted = value - 3;
+          resolve(subtracted);
+        }, 1000);
+      });
+    }
+
+    function DividedByTwo(value) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const divided = value / 2;
+          resolve(divided);
+        }, 1000);
+      });
+    }
+
+    function AddTen(value) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const added = value + 10;
+          resolve(added);
+        }, 1000);
+      });
+    }
+
+    btn.addEventListener("click", () => {
+      const inputValue = parseFloat(document.getElementById("ip").value);
+
+
+      MultipliedByTwo(inputValue)
+        .then(res1 => {
+          result.innerHTML += `<br>Result: ${res1}`;
+          return SubtractThree(res1);
+        })
+        .then(res2 => {
+          result.innerHTML += `<br>Result: ${res2}`;
+          return DividedByTwo(res2);
+        })
+        .then(res3 => {
+          result.innerHTML += `<br>Result: ${res3}`;
+          return AddTen(res3);
+        })
+        .then(finalRes => {
+          result.innerHTML += `<br>Result: ${finalRes}`;
+        })
+        .catch(err => {
+          result.innerHTML = `<span style="color:red">Error: ${err}</span>`;
+        });
+    });
